b41a32ae79dac00d89a1ff0adc6f5a17c4ae5d8e
FlyBird-S <eesongpengfei@foxmail.com>
2018-01-19 23:31:19 +0800

    django test OK

--- Django/myblog/blog/admin.py
+++ Django/myblog/blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from . import models
-admin.site.register(models.Article)
+
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content','pub_time')
+    list_filter = ('pub_time',)
+    # def __init__(self):
+    #     self.list_display = ('title','content')
+admin.site.register(models.Article,ArticleAdmin)
